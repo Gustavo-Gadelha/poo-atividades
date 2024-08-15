389c137300c3e45fc22f729a38fdd2fdef4ca666
Gustavo-Gadelha <gustavofg2112@gmail.com>
2024-08-15 14:48:32 -0300

    Added atividade16

---
+++ src/atividade16/model/Pessoa.java
@@ -0,0 +1,21 @@
+package atividade16.model;
+
+import java.util.ArrayList;
+
+public class Pessoa {
+    public String nome;
+    public String cpf;
+    public static int quantidadeDePessoas;
+    public static ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+        Pessoa.quantidadeDePessoas += 1;
+        Pessoa.listaDePessoas.add(this);
+    }
+
+    public void digaOi() {
+        System.out.println(this.nome + " disse oi");
+    }
+}
